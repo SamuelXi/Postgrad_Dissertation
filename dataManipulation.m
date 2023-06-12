df08fb4f18cac644754c6d51a82c23c3093de26f
Samuel <xisunbowen0922@gmail.com>
2023-06-12 17:21:08 +0100

    Add dataManipulation script
    Including data import from folder, rtAvg calculation (fixed NaN) could start on model fitting testing

---
+++ dataManipulation.m
@@ -0,0 +1,73 @@
+%% Extracting data from the file
+clc, clearvars
+%folder path of the individual data
+folderPath = '/Users/samuelxisun/Desktop/Experiment/individual_data_paper';
+
+% List all .csv file under the individual_data_paper folder
+% should be 115 participants
+files = dir(fullfile(folderPath, '*.csv'));
+
+% All partitcipants file, 115*148*3
+allParticipants = zeros(numel(files),148, 3)
+
+%displat all file contents
+for i = 1:numel(files)
+    filePath = fullfile(folderPath, files(i).name);
+    % Read the file
+    fileData = readtable(filePath);
+
+    %check number of rows for each individual files
+    tableSize = size(fileData);
+    numRows = tableSize(1);
+    numcols = tableSize(2);
+
+    %set individual input data matrix
+    inputIndData = zeros(numRows,3);
+    
+    %extract data from each file
+    %1st column condition, 2nd column response type, 3rd column rt
+    inputIndData(:,1) = fileData.gamble;
+    inputIndData(:,2) = fileData.win;
+    inputIndData(:,3) = fileData.rt / 1000; % reaction time have to be in sec
+
+    % condition = inputIndData(:,1) == -999.000;
+    % inputIntData(condition, :) = [];
+    % disp(filePath);
+    % disp(inputIndData);
+
+    %append individual data to the all data matrix
+    allParticipants(i,:,:) = inputIndData;
+    disp(allParticipants)
+end
+
+
+%% Deal with NaN situation
+%ignoring the function by using omitmissing in mean function
+% allParticipants(isnan(allParticipants)) = 0
+
+
+%% clean up data that participants exceed the time
+%remove all -999, by replacing the rt from NaN to 0
+allparticipantsClean = [];
+for j = 1:numel(files)
+    for k = 1:numRows
+        if allParticipants(j,k,1) ~= -999;
+            allParticipantsClean(j,k,:) = allParticipants(j, k, :);
+        else
+            disp(j)
+        end 
+    end
+end
+
+%% Calculating avg
+%create a 115*1 matrix to store each participants avg reaction time
+rtAvg = zeros(115,1)
+for i = 1:numel(files)
+    rtAvg(i) = mean(allParticipants(i,:,3),"omitmissing");
+end
+disp(rtAvg)
+    
+
+
+
+
