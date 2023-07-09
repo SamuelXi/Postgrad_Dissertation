30a4121a4d044e4bee37c7a166ce34f5891bf441
Samuel <xisunbowen0922@gmail.com>
2023-07-09 11:29:45 +0100

    Update on T-test file
    Finish implementing New T-test on separate fitted data, significant results found

--- ANOVATestingScript.m
+++ ANOVATestingScript.m
@@ -11,23 +11,11 @@ con2 = readtable(filePathConR2);
 con3 = readtable(filePathConR3);
 con4 = readtable(filePathConR4);
 con5 = readtable(filePathConR5);
+ConSep5_101 = readtable(filePathConR5_101);
+ConSep5_102 = readtable(filePathConR5_102);
 
 
-%% Testing for significance between different model
-
-%Read table
-fileDataT1 = readtable(filePathNoConR1);
-fileDataT2 = readtable(filePathNoConR5);
-
-T1 = fileDataT1.z;
-T2 = fileDataT2.z;
-test = [T1,T2]
-% [h, p, ci, stats] = ttest2(T1,T2);
-
-%% Comparing between anxious and non annxious group in all parameter values
-% Create testing group for based on the on anxious and not anxious
-% Using all free parameters model data
-
+%% Parameters setup
 % No condition group
 vNoAnxG1 = [];
 vAnxG1 = [];
@@ -60,9 +48,73 @@ stAnxG2 = [];
 etaNoAnxG2 = [];
 etaAnxG2 = [];
 
-% Group size
-anxSize = size(anxPar);
-anxNoSize = size(anxNoPar);
+% Condition for Seperation
+vNoAnxG2_Sep_101 = [];
+vAnxG2_Sep_101 = [];
+aNoAnxG2_Sep_101 = [];
+aAnxG2_Sep_101 = [];
+zNoAnxG2_Sep_101 = [];
+zAnxG2_Sep_101 = [];
+TerNoAnxG2_Sep_101 = [];
+TerAnxG2_Sep_101 = [];
+szNoAnxG2_Sep_101 = [];
+szAnxG2_Sep_101 = [];
+stNoAnxG2_Sep_101 = [];
+stAnxG2_Sep_101 = [];
+etaNoAnxG2_Sep_101 = [];
+etaAnxG2_Sep_101 = [];
+
+vNoAnxG2_Sep_102 = [];
+vAnxG2_Sep_102 = [];
+aNoAnxG2_Sep_102 = [];
+aAnxG2_Sep_102 = [];
+zNoAnxG2_Sep_102 = [];
+zAnxG2_Sep_102 = [];
+TerNoAnxG2_Sep_102 = [];
+TerAnxG2_Sep_102 = [];
+szNoAnxG2_Sep_102 = [];
+szAnxG2_Sep_102 = [];
+stNoAnxG2_Sep_102 = [];
+stAnxG2_Sep_102 = [];
+etaNoAnxG2_Sep_102 = [];
+etaAnxG2_Sep_102 = [];
+
+v_Sep_101 = [];
+a_Sep_101 = [];
+z_Sep_101 = [];
+Ter_Sep_101 = [];
+sz_Sep_101 = [];
+st_Sep_101 = [];
+eta_Sep_101 = [];
+
+v_Sep_102 = [];
+a_Sep_102 = [];
+z_Sep_102 = [];
+Ter_Sep_102 = [];
+sz_Sep_102 = [];
+st_Sep_102 = [];
+eta_Sep_102 = [];
+
+v_Sep_101 = ConSep5_101(:,:).v;
+a_Sep_101 = ConSep5_101(:,:).a;
+z_Sep_101 = ConSep5_101(:,:).z;
+Ter_Sep_101 = ConSep5_101(:,:).Ter;
+sz_Sep_101 = ConSep5_101(:,:).sz;
+st_Sep_101 = ConSep5_101(:,:).st;
+eta_Sep_101 = ConSep5_101(:,:).eta;
+v_Sep_102 = ConSep5_102(:,:).v;
+a_Sep_102 = ConSep5_102(:,:).a;
+z_Sep_102 = ConSep5_102(:,:).z;
+Ter_Sep_102 = ConSep5_102(:,:).Ter;
+sz_Sep_102 = ConSep5_102(:,:).sz;
+st_Sep_102 = ConSep5_102(:,:).st;
+eta_Sep_102 = ConSep5_102(:,:).eta;
+
+
+%% Comparing between anxious and non annxious group in all parameter values
+% % Group size
+% anxSize = size(anxPar);
+% anxNoSize = size(anxNoPar);
 
 % Extract participant information
 for i = 1:115
@@ -133,9 +185,79 @@ for i = 1:115
         %Extract eta value
         etaAnxG2 = [etaAnxG2,con5(i,:).eta_101_];
     end
+
+    %Condtion Separated 101
+    if ismember(ConSep5_101(i,:).participantsID, anxNoPar)
+        %Extract v value
+        vNoAnxG2_Sep_101 = [vNoAnxG2_Sep_101,ConSep5_101(i,:).v];
+        %Extract a value
+        aNoAnxG2_Sep_101 = [aNoAnxG2_Sep_101,ConSep5_101(i,:).a];
+        %Extract z value
+        zNoAnxG2_Sep_101 = [zNoAnxG2_Sep_101,ConSep5_101(i,:).z];
+        %Extract Ter value
+        TerNoAnxG2_Sep_101 = [TerNoAnxG2_Sep_101,ConSep5_101(i,:).Ter];
+        %Extract sz value
+        szNoAnxG2_Sep_101 = [szNoAnxG2_Sep_101,ConSep5_101(i,:).sz];
+        %Extract st value
+        stNoAnxG2_Sep_101 = [stNoAnxG2_Sep_101,ConSep5_101(i,:).st];
+        %Extract eta value
+        etaNoAnxG2_Sep_101 = [etaNoAnxG2_Sep_101,ConSep5_101(i,:).eta];
+
+    elseif ismember(ConSep5_101(i,:).participantsID, anxPar)
+        %Extract v value
+        vAnxG2_Sep_101 = [vAnxG2_Sep_101,ConSep5_101(i,:).v];
+        %Extract a value
+        aAnxG2_Sep_101 = [aAnxG2_Sep_101,ConSep5_101(i,:).a];
+        %Extract z value
+        zAnxG2_Sep_101 = [zAnxG2_Sep_101,ConSep5_101(i,:).z];
+        %Extract Ter value
+        TerAnxG2_Sep_101 = [TerAnxG2_Sep_101,ConSep5_101(i,:).Ter];
+        %Extract sz value
+        szAnxG2_Sep_101 = [szAnxG2_Sep_101,ConSep5_101(i,:).sz];
+        %Extract st value
+        stAnxG2_Sep_101 = [stAnxG2_Sep_101,ConSep5_101(i,:).st];
+        %Extract eta value
+        etaAnxG2_Sep_101 = [etaAnxG2_Sep_101,ConSep5_101(i,:).eta];
+    end
+
+    %Condtion Separated 102
+    if ismember(ConSep5_102(i,:).participantsID, anxNoPar)
+        %Extract v value
+        vNoAnxG2_Sep_102 = [vNoAnxG2_Sep_102,ConSep5_102(i,:).v];
+        %Extract a value
+        aNoAnxG2_Sep_102 = [aNoAnxG2_Sep_102,ConSep5_102(i,:).a];
+        %Extract z value
+        zNoAnxG2_Sep_102 = [zNoAnxG2_Sep_102,ConSep5_102(i,:).z];
+        %Extract Ter value
+        TerNoAnxG2_Sep_102 = [TerNoAnxG2_Sep_102,ConSep5_102(i,:).Ter];
+        %Extract sz value
+        szNoAnxG2_Sep_102 = [szNoAnxG2_Sep_102,ConSep5_102(i,:).sz];
+        %Extract st value
+        stNoAnxG2_Sep_102 = [stNoAnxG2_Sep_102,ConSep5_102(i,:).st];
+        %Extract eta value
+        etaNoAnxG2_Sep_102 = [etaNoAnxG2_Sep_102,ConSep5_102(i,:).eta];
+
+    elseif ismember(ConSep5_102(i,:).participantsID, anxPar)
+        %Extract v value
+        vAnxG2_Sep_102 = [vAnxG2_Sep_102,ConSep5_102(i,:).v];
+        %Extract a value
+        aAnxG2_Sep_102 = [aAnxG2_Sep_102,ConSep5_102(i,:).a];
+        %Extract z value
+        zAnxG2_Sep_102 = [zAnxG2_Sep_102,ConSep5_102(i,:).z];
+        %Extract Ter value
+        TerAnxG2_Sep_102 = [TerAnxG2_Sep_102,ConSep5_102(i,:).Ter];
+        %Extract sz value
+        szAnxG2_Sep_102 = [szAnxG2_Sep_102,ConSep5_102(i,:).sz];
+        %Extract st value
+        stAnxG2_Sep_102 = [stAnxG2_Sep_102,ConSep5_102(i,:).st];
+        %Extract eta value
+        etaAnxG2_Sep_102 = [etaAnxG2_Sep_102,ConSep5_102(i,:).eta];
+    end
+
 end
 
-% T-test setup
+
+%% T-test setup
 % v parameter
 [h1, p1, ci1, stats1] = ttest2(vNoAnxG1,vAnxG1); %no Condition
 [h2, p2, ci2, stats2] = ttest2(vNoAnxG2,vAnxG2); %Condition
@@ -163,6 +285,7 @@ end
 % eta parameter
 [h13, p13, ci13, stats13] = ttest2(etaNoAnxG1,etaAnxG1); %no Condition
 [h14, p14, ci14, stats14] = ttest2(etaNoAnxG2,etaAnxG2); %Condition
+
 
 
 % Display the results
@@ -236,6 +359,323 @@ fprintf('t-statistic: %.4f\n', stats14.tstat);
 fprintf('p-value: %.4f\n', p14);
 fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci14(1), ci14(2));
 fprintf("\n");
+
+%% Seperate test
+% Compare 101 and 102 for all value 
+% v parameter
+[h15, p15, ci15, stats15] = ttest2(v_Sep_101,v_Sep_102); 
+
+% a parameter
+[h16, p16, ci16, stats16] = ttest2(a_Sep_101,a_Sep_102); 
+
+% z parameter
+[h17, p17, ci17, stats17] = ttest2(z_Sep_101,z_Sep_102); 
+
+% Ter parameter
+[h18, p18, ci18, stats18] = ttest2(Ter_Sep_101,Ter_Sep_102); 
+
+% sz parameter
+[h19, p19, ci19, stats19] = ttest2(sz_Sep_101,sz_Sep_101); 
+
+% st parameter
+[h20, p20, ci20, stats20] = ttest2(st_Sep_101,st_Sep_102); 
+
+% eta parameter
+[h21, p21, ci21, stats21] = ttest2(eta_Sep_101,eta_Sep_102); 
+fprintf('Compare 101 and 102 for all value\n')
+fprintf('______________Condition(Seperate fitting) v t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats15.tstat);
+fprintf('p-value: %.4f\n', p15);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci15(1), ci15(2));
+fprintf("\n");
+fprintf('______________Condition(Seperate fitting) a t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats16.tstat);
+fprintf('p-value: %.4f\n', p16);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci16(1), ci16(2));
+fprintf("\n");
+fprintf('______________Condition(Seperate fitting) z t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats17.tstat);
+fprintf('p-value: %.4f\n', p17);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci17(1), ci17(2));
+fprintf("\n");
+fprintf('_____________Condition(Seperate fitting) Ter t-test______________\n');
+fprintf('t-statistic: %.4f\n', stats18.tstat);
+fprintf('p-value: %.4f\n', p18);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci18(1), ci18(2));
+fprintf("\n");
+fprintf('_____________Condition(Seperate fitting) sz t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats19.tstat);
+fprintf('p-value: %.4f\n', p19);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci19(1), ci19(2));
+fprintf("\n");
+fprintf('_____________Condition(Seperate fitting) st t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats20.tstat);
+fprintf('p-value: %.4f\n', p20);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci20(1), ci20(2));
+fprintf("\n");
+fprintf('_____________Condition(Seperate fitting) eta t-test______________\n');
+fprintf('t-statistic: %.4f\n', stats21.tstat);
+fprintf('p-value: %.4f\n', p21);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci21(1), ci21(2));
+fprintf("\n");
+
+
+%% Compare between group 101 anx and Non anx
+
+% T test setup 
+% v parameter
+[h22, p22, ci22, stats22] = ttest2(vNoAnxG2_Sep_101,vAnxG2_Sep_101); 
+
+% a parameter
+[h23, p23, ci23, stats23] = ttest2(aNoAnxG2_Sep_101,aAnxG2_Sep_101); 
+
+% z parameter
+[h24, p24, ci24, stats24] = ttest2(zNoAnxG2_Sep_101,zAnxG2_Sep_101); 
+
+% Ter parameter
+[h25, p25, ci25, stats25] = ttest2(TerNoAnxG2_Sep_101,TerAnxG2_Sep_101); 
+
+% sz parameter
+[h26, p26, ci26, stats26] = ttest2(szNoAnxG2_Sep_101,szAnxG2_Sep_101); 
+
+% st parameter
+[h27, p27, ci27, stats27] = ttest2(stNoAnxG2_Sep_101,stAnxG2_Sep_101); 
+
+% eta parameter
+[h28, p28, ci28, stats28] = ttest2(etaNoAnxG2_Sep_101,etaAnxG2_Sep_101); 
+
+fprintf('Compare between group 101 anx and Non anx\n')
+fprintf('______________101 Anx & Non Anx(Seperate fitting) v t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats22.tstat);
+fprintf('p-value: %.4f\n', p22);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci22(1), ci22(2));
+fprintf("\n");
+fprintf('______________101 Anx & Non Anx(Seperate fitting) a t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats23.tstat);
+fprintf('p-value: %.4f\n', p23);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci23(1), ci23(2));
+fprintf("\n");
+fprintf('______________101 Anx & Non Anx(Seperate fitting) z t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats24.tstat);
+fprintf('p-value: %.4f\n', p24);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci24(1), ci24(2));
+fprintf("\n");
+fprintf('_____________101 Anx & Non Anx(Seperate fitting) Ter t-test______________\n');
+fprintf('t-statistic: %.4f\n', stats25.tstat);
+fprintf('p-value: %.4f\n', p25);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci25(1), ci25(2));
+fprintf("\n");
+fprintf('_____________101 Anx & Non Anx(Seperate fitting) sz t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats26.tstat);
+fprintf('p-value: %.4f\n', p26);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci26(1), ci26(2));
+fprintf("\n");
+fprintf('_____________101 Anx & Non Anx(Seperate fitting) st t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats27.tstat);
+fprintf('p-value: %.4f\n', p27);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci27(1), ci27(2));
+fprintf("\n");
+fprintf('_____________101 Anx & Non Anx(Seperate fitting) eta t-test______________\n');
+fprintf('t-statistic: %.4f\n', stats28.tstat);
+fprintf('p-value: %.4f\n', p28);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci28(1), ci28(2));
+fprintf("\n");
+
+%% Compare between group 102 anx and Non anx
+
+% T test setup 
+% v parameter
+[h29, p29, ci29, stats29] = ttest2(vNoAnxG2_Sep_102,vAnxG2_Sep_102); 
+
+% a parameter
+[h30, p30, ci30, stats30] = ttest2(aNoAnxG2_Sep_102,aAnxG2_Sep_102); 
+
+% z parameter
+[h31, p31, ci31, stats31] = ttest2(zNoAnxG2_Sep_102,zAnxG2_Sep_102); 
+
+% Ter parameter
+[h32, p32, ci32, stats32] = ttest2(TerNoAnxG2_Sep_102,TerAnxG2_Sep_102); 
+
+% sz parameter
+[h33, p33, ci33, stats33] = ttest2(szNoAnxG2_Sep_102,szAnxG2_Sep_102); 
+
+% st parameter
+[h34, p34, ci34, stats34] = ttest2(stNoAnxG2_Sep_102,stAnxG2_Sep_102); 
+
+% eta parameter
+[h35, p35, ci35, stats35] = ttest2(etaNoAnxG2_Sep_102,etaAnxG2_Sep_102); 
+
+fprintf('Compare between group 102 anx and Non anx\n')
+fprintf('______________102 Anx & Non Anx(Seperate fitting) v t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats29.tstat);
+fprintf('p-value: %.4f\n', p29);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci29(1), ci29(2));
+fprintf("\n");
+fprintf('______________102 Anx & Non Anx(Seperate fitting) a t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats30.tstat);
+fprintf('p-value: %.4f\n', p30);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci30(1), ci30(2));
+fprintf("\n");
+fprintf('______________102 Anx & Non Anx(Seperate fitting) z t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats31.tstat);
+fprintf('p-value: %.4f\n', p31);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci31(1), ci31(2));
+fprintf("\n");
+fprintf('_____________102 Anx & Non Anx(Seperate fitting) Ter t-test______________\n');
+fprintf('t-statistic: %.4f\n', stats32.tstat);
+fprintf('p-value: %.4f\n', p32);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci32(1), ci32(2));
+fprintf("\n");
+fprintf('_____________102 Anx & Non Anx(Seperate fitting) sz t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats33.tstat);
+fprintf('p-value: %.4f\n', p33);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci33(1), ci33(2));
+fprintf("\n");
+fprintf('_____________102 Anx & Non Anx(Seperate fitting) st t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats34.tstat);
+fprintf('p-value: %.4f\n', p34);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci27(1), ci27(2));
+fprintf("\n");
+fprintf('_____________102 Anx & Non Anx(Seperate fitting) eta t-test______________\n');
+fprintf('t-statistic: %.4f\n', stats35.tstat);
+fprintf('p-value: %.4f\n', p35);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci35(1), ci35(2));
+fprintf("\n");
+
+
+%% Compare between group 101 and 102 anx
+
+% T test setup 
+% v parameter
+[h36, p36, ci36, stats36] = ttest2(vAnxG2_Sep_101,vAnxG2_Sep_102); 
+
+% a parameter
+[h37, p37, ci37, stats37] = ttest2(aAnxG2_Sep_101,aAnxG2_Sep_102); 
+
+% z parameter
+[h38, p38, ci38, stats38] = ttest2(zAnxG2_Sep_101,zAnxG2_Sep_102); 
+
+% Ter parameter
+[h39, p39, ci39, stats39] = ttest2(TerAnxG2_Sep_101,TerAnxG2_Sep_102); 
+
+% sz parameter
+[h40, p40, ci40, stats40] = ttest2(szAnxG2_Sep_101,szAnxG2_Sep_102); 
+
+% st parameter
+[h41, p41, ci41, stats41] = ttest2(stAnxG2_Sep_101,stAnxG2_Sep_102); 
+
+% eta parameter
+[h42, p42, ci42, stats42] = ttest2(etaAnxG2_Sep_101,etaAnxG2_Sep_102); 
+
+fprintf('Compare between group 101 and 102 anx\n')
+fprintf('______________101 and 102 anx(Seperate fitting) v t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats36.tstat);
+fprintf('p-value: %.4f\n', p36);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci36(1), ci36(2));
+fprintf("\n");
+fprintf('______________101 and 102 anx(Seperate fitting) a t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats37.tstat);
+fprintf('p-value: %.4f\n', p37);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci37(1), ci37(2));
+fprintf("\n");
+fprintf('______________101 and 102 anx(Seperate fitting) z t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats38.tstat);
+fprintf('p-value: %.4f\n', p38);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci38(1), ci38(2));
+fprintf("\n");
+fprintf('_____________101 and 102 anx(Seperate fitting) Ter t-test______________\n');
+fprintf('t-statistic: %.4f\n', stats39.tstat);
+fprintf('p-value: %.4f\n', p39);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci39(1), ci39(2));
+fprintf("\n");
+fprintf('_____________101 and 102 anx(Seperate fitting) sz t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats40.tstat);
+fprintf('p-value: %.4f\n', p40);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci40(1), ci40(2));
+fprintf("\n");
+fprintf('_____________101 and 102 anx(Seperate fitting) st t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats41.tstat);
+fprintf('p-value: %.4f\n', p41);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci41(1), ci41(2));
+fprintf("\n");
+fprintf('_____________101 and 102 anx(Seperate fitting) eta t-test______________\n');
+fprintf('t-statistic: %.4f\n', stats42.tstat);
+fprintf('p-value: %.4f\n', p42);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci42(1), ci42(2));
+fprintf("\n");
+
+%% Compare between group 101 and 102 Non anx
+
+% T test setup 
+% v parameter
+[h43, p43, ci43, stats43] = ttest2(vNoAnxG2_Sep_101,vNoAnxG2_Sep_102); 
+
+% a parameter
+[h44, p44, ci44, stats44] = ttest2(aNoAnxG2_Sep_101,aNoAnxG2_Sep_102); 
+
+% z parameter
+[h45, p45, ci45, stats45] = ttest2(zNoAnxG2_Sep_101,zNoAnxG2_Sep_102); 
+
+% Ter parameter
+[h46, p46, ci46, stats46] = ttest2(TerNoAnxG2_Sep_101,TerNoAnxG2_Sep_102); 
+
+% sz parameter
+[h47, p47, ci47, stats47] = ttest2(szNoAnxG2_Sep_101,szNoAnxG2_Sep_102); 
+
+% st parameter
+[h48, p48, ci48, stats48] = ttest2(stNoAnxG2_Sep_101,stNoAnxG2_Sep_102); 
+
+% eta parameter
+[h49, p49, ci49, stats49] = ttest2(etaNoAnxG2_Sep_101,etaNoAnxG2_Sep_102); 
+
+fprintf('Compare between group 101 and 102 Non anx\n')
+fprintf('______________101 and 102 Non anx(Seperate fitting) v t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats43.tstat);
+fprintf('p-value: %.4f\n', p43);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci43(1), ci43(2));
+fprintf("\n");
+fprintf('______________101 and 102 Non anx(Seperate fitting) a t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats44.tstat);
+fprintf('p-value: %.4f\n', p44);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci44(1), ci44(2));
+fprintf("\n");
+fprintf('______________101 and 102 Non anx(Seperate fitting) z t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats45.tstat);
+fprintf('p-value: %.4f\n', p45);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci45(1), ci45(2));
+fprintf("\n");
+fprintf('_____________101 and 102 Non anx(Seperate fitting) Ter t-test______________\n');
+fprintf('t-statistic: %.4f\n', stats46.tstat);
+fprintf('p-value: %.4f\n', p46);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci46(1), ci46(2));
+fprintf("\n");
+fprintf('_____________101 and 102 Non anx(Seperate fitting) sz t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats47.tstat);
+fprintf('p-value: %.4f\n', p47);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci47(1), ci47(2));
+fprintf("\n");
+fprintf('_____________101 and 102 Non anx(Seperate fitting) st t-test_______________\n');
+fprintf('t-statistic: %.4f\n', stats48.tstat);
+fprintf('p-value: %.4f\n', p48);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci48(1), ci48(2));
+fprintf("\n");
+fprintf('_____________101 and 102 Non anx(Seperate fitting) eta t-test______________\n');
+fprintf('t-statistic: %.4f\n', stats49.tstat);
+fprintf('p-value: %.4f\n', p49);
+fprintf('95%% Confidence Interval: [%.4f, %.4f]\n', ci49(1), ci49(2));
+fprintf("\n");
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
